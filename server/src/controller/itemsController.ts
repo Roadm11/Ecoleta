cf7251707efea202428fbdc3c44314f9cba18812
Roadm11 <rodrigo.adm10@hotmail.com>
2020-07-06 01:26:45 -0300

    Subida do projeto Ecoleta desenvolvido na NLW_01

---
+++ server/src/controller/itemsController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+import knex from "../database/connection";
+
+class ItemsController {
+    async index (req: Request, resp: Response){
+        const items = await knex('items').select('*');
+    
+        const serializedItems = items.map(item => {
+            return {
+                id: item.id,
+                title: item.title,
+                image_url: `http://192.168.0.66:3333/uploads/${item.image}`,
+            }
+        });
+    
+        return resp.json(serializedItems);
+    };
+};
+
+export default ItemsController;
